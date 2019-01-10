57f66b997dd2277001c3c5a7bacabef316f9ebfa
Joshua Fernandes <joshferns0212@gmail.com>
2019-01-09 22:44:17 -0500

    Another One

---
+++ multiline.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+
+//Joshua Fernandes
+using namespace std;
+
+int main()
+{
+    int num = 2;
+
+    switch (num)
+    {
+       case 1:
+        cout << num;
+        break;
+       case 2:
+        cout << num;
+        break;
+       case 3:
+        cout << num;
+        break;
+       case 4:
+        cout << num;
+        break;
+    }
+    
+
+
+
+    return 0;
+}
