1da5c449355bb04a79874c19f43bc22046700a26
Joshua Fernandes <joshferns0212@gmail.com>
2019-01-14 22:15:25 -0500

    sd

--- Test.java
+++ Test.java
@@ -1,13 +1,19 @@
-public class Test extends JFrame implements KeyListener
+import javax.swing.*;
+import java.awt.*;
+
+public class Test extends JFrame
 {
+    static final long serialVersionUID = 0L;
    public Test()
    {
        super("Test");
        this.setBackground(Color.white);
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       this.setSize(/*x size*/, /*y size*/);
+       this.setSize(200, 200);
        this.setVisible(true);
-       this.setLocation(/*x location*/, /*y location*/);
-       this.addKeyListener(this);
+       this.setLocation(0, 0);
+   }
+   public static void main(String[] args) {
+       new Test();
    }
 }
