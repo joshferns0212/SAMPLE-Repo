9f33a5c5534a26c50e9b2bb489ead2afb77a13da
Joshua Fernandes <joshferns0212@gmail.com>
2019-02-09 22:30:11 -0500

    indentations

--- Test.java
+++ Test.java
@@ -3,17 +3,17 @@ import java.awt.*;
 
 public class Test extends JFrame
 {
-    static final long serialVersionUID = 0L;
-   public Test()
-   {
-       super("Test");
-       this.setBackground(Color.white);
-       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       this.setSize(200, 200);
-       this.setVisible(true);
-       this.setLocation(0, 0);
-   }
-   public static void main(String[] args) {
-       new Test();
-   }
+	static final long serialVersionUID = 0L;
+	public Test()
+	{
+		super("Test");
+		this.setBackground(Color.white);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(200, 200);
+		this.setVisible(true);
+		this.setLocation(0, 0);
+	}
+	public static void main(String[] args) {
+		new Test();
+	}
 }
--- multiline.cpp
+++ multiline.cpp
@@ -9,35 +9,35 @@ using namespace std;
 
 int main()
 {
-    int num = 2;
+	int num = 2;
 
-    switch (num)
-    {
-    case 1:
-        cout << num;
-        break;
-    case 2:
-        cout << num;
-        break;
-    case 3:
-        cout << num;
-        break;
-    case 4:
-        cout << num;
-        break;
-    }
+	switch (num)
+	{
+	case 1:
+		cout << num;
+		break;
+	case 2:
+		cout << num;
+		break;
+	case 3:
+		cout << num;
+		break;
+	case 4:
+		cout << num;
+		break;
+	}
 
-    int i[] = {1,2,3,4,5};
-    
-    
-    
-    for(int n : i)
-    {
-        cout << n;
-    }
-    
-    
-    
-    
-    return 0;
+	int i[] = {1,2,3,4,5};
+
+
+
+	for(int n : i)
+	{
+		cout << n;
+	}
+
+
+
+
+	return 0;
 }
