48279a1458c087af5439ba2b5b0bf7b0b743edf7
Joshua Fernandes <joshferns0212@gmail.com>
2019-04-17 23:48:52 -0400

    Added template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+	string text;
+	cin >> text;
+	cout << text;
+	system ("PAUSE");
+	return EXIT_SUCCESS;
+}
